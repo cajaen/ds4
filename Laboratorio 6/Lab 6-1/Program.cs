3a8f03f271a058f6da6c4cccdebd264e3d27edde
Carlos Jaen <cajaen17@gmail.com>
2024-09-17 18:54:35 -0500

    Laboratorio 6 culminado

---
+++ Laboratorio 6/Lab 6-1/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int num = -1;
+        Console.WriteLine("Digite el número deseado");
+        try
+        {
+            num = Int16.Parse(Console.ReadLine());
+        }
+        catch (FormatException ex)
+        {
+            Console.WriteLine("No ha introducido un dígito válido");
+        }
+
+        Console.WriteLine(num);
+    }
+}
---
+++ Laboratorio 6/Laboratorio 6/Program.cs
@@ -0,0 +1,2 @@
+﻿// Veamos las siguientes maneras en que podemos gestionar las excepciones en c# (todos los ejemplos 
+//partiendo desde la clase Program generada por default al crear un nuevo proyecto//
